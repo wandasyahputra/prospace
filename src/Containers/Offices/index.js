2c9c89e54eff4416756aa86b8666b39f441cbf0f
wandasyahputra <wanda@qlue.id>
2018-07-08 21:14:15 +0700

    dummy layout for offices

--- src/Containers/Offices/index.js
+++ src/Containers/Offices/index.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import {Box} from "../../Components"
+import './Offices.css'
 
 class Offices extends Component {
   render() {
     return (
-      <div className="Offices">
-        <div className="row">
-          <div className="col-md-6">
-            <h3>Create Company</h3>
-
+      <div className="container">
+        <div className="Offices">
+          <div className="row">
+              <div className="header">
+                <h4>Google</h4>
+              </div>
+          </div>
+          <div className="row content-container">
+            <div className="content">
+              <div className="content-label">Label1</div>
+              <div className="content-body">Pernyataan 1</div>
+            </div>
+            <div className="content">
+              <div className="content-label">Label1</div>
+              <div className="content-body">Pernyataan 1</div>
+            </div>
+            <div className="content">
+              <div className="content-label">Label1</div>
+              <div className="content-body">Pernyataan 1</div>
+            </div>
+            <div className="content">
+              <div className="content-label">Label1</div>
+              <div className="content-body">Pernyataan 1</div>
+            </div>
+            <a className="btn btn-primary bottomRight-absolute" href="Overviews">Back To Overviews</a>
+          </div>
+          <div className="row box-container">
+            <Box/>
+            <Box/>
+            <Box/>
+            <Box/>
           </div>
         </div>
       </div>
